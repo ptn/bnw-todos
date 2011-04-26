a6f04f90a63e396c957e5985e02fe5b50e895601
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-26 10:26:07 -0500

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BnwTodos::Application.config.secret_token = 'a993b2e58e9b6f9f6d8695fc855e43258db460f3a4f4925995e1644a04cb1b3ec4e8327b19fbb6d0e0978a7993815a83542034a0df8cd8d0aae94177f9d0e96e'
