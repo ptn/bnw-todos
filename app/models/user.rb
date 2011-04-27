0c7cb6c16ef057789105c6569a425cd9c352386b
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-26 22:48:11 -0500

    Added recipe for salted hashed passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
+require 'digest/sha1'
+
 class User < ActiveRecord::Base
+  attr_accessor :password_confirmation
+  validates :password, :confirmation => true
+
+  def password
+    @password
+  end
+
+  def password=(pwd)
+    @password = pwd
+    return if pwd.blank?
+    create_salt
+    self.hashed_password = User.encrypt_password self.password, self.salt
+  end
+
+
+  private
+
+  def self.encrypt_password(pwd, salt)
+    Digest::SHA1.hexdigest(pwd + salt)
+  end
+
+  def create_salt
+    self.salt = self.object_id.to_s + rand.to_s
+  end
 end
