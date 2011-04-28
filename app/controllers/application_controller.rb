f104486011d360cb4cbab8e90184ac3dbd3a138d
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-28 16:07:15 -0500

    Added current_user method to AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  private
+
+  def current_user
+    @current_user = session[:current_user_id] && User.find(session[:current_user_id])
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def new
-    unless session[:user_id].nil?
+    unless session[:current_user_id].nil?
       flash[:warn] = "You are already logged in"
       redirect_to :back
     end
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:username], params[:password])
     respond_to do |format|
       if user
-        session[:user_id] = user.id
+        session[:current_user_id] = user.id
         format.html { redirect_to projects_path }
       else
         flash[:error] = "Incorrect username or password"
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    session[:current_user_id] = nil
     redirect_to :action => :new
   end
 
