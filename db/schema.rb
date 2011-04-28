64b4a6e03b600031274901618067a73868e6883d
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-28 15:23:56 -0500

    Create participants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110427163914) do
+ActiveRecord::Schema.define(:version => 20110427170310) do
 
   create_table "lists", :force => true do |t|
     t.integer  "project_id"
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "participants", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
