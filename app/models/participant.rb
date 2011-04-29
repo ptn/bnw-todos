da24518eabe2ee91b3d05337a8ae9cffe0795e81
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-28 23:51:47 -0500

    Fix typo

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,6 @@
 class Participant < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  has_many :todos, :foregin_key => "assignee_id"
+  has_many :todos, :foreign_key => "assignee_id"
   delegate :username, :to => :user
 end
