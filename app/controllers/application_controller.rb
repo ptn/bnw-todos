0bb31884a7a5f025da8fdfec69917b15b30557f0
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-29 02:03:47 -0500

    Tiny fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def current_user
-    @current_user = session[:current_user_id] && User.find(session[:current_user_id])
+    @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
   end
 
   def require_login
