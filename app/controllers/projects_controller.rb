b8ac567c4cf2dd1961ca314d74bbe85739f58e43
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-30 17:24:43 -0500

    Toggle forms for new todos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
   # GET /projects/1.xml
   def show
     @project = Project.find(params[:id])
+    @new_todo = Todo.new
 
     respond_to do |format|
       format.html # show.html.erb
