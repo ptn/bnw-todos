af7579c99cf2472abd8fb6dc4ffbcc3c9d53df9c
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-05-04 00:37:14 -0500

    Added List.from_project_where_user_participates

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,16 @@
 class List < ActiveRecord::Base
   scope :done, where(:done => true)
   scope :left, where(:done => false)
+  #FIXME Should return an ActiveRecord Relation
+  scope :from_project_where_user_participates, lambda { |project, user|
+    participant = Participant.where(:user_id => user.id, :project_id => project.id).first
+    lists = nil
+    if participant
+      lists = project.lists.select { |list| list.todos.any? { |todo| todo.assignee == participant } }
+    end
+    lists
+  }
+
 
   belongs_to :project
   has_many :todos, :dependent => :destroy
